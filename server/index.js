a94b6692b2da21baf669db7bca679f9410ca4b16
Sriman <121006408+Kevin-0-0@users.noreply.github.com>
2024-09-04 00:22:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 //allow access to front end
-app.use(cors({origin:['https://sociopedia-client-one.vercel.app']}));
+// app.use(cors({origin:['https://sociopedia-client-one.vercel.app']}));
 
 app.use(express.json());
 app.use(helmet());
