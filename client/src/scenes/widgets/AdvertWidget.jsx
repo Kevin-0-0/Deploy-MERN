1569fdcc5a17bffa895f2587691835c5953577aa
Sriman <121006408+Kevin-0-0@users.noreply.github.com>
2024-09-06 14:53:11 +0530

    Update AdvertWidget.jsx

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="https://deploy-mern-api-weld.vercel.app/assets/info4.jpeg"
+        src="https://api-one-lyart-74.vercel.app/assets/info4.jpeg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
