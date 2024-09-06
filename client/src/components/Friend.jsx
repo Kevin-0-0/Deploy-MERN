9d3325449adb75276e697d73919ba1a9a6b54a23
Sriman <121006408+Kevin-0-0@users.noreply.github.com>
2024-09-06 13:12:08 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://deploy-mern-api-sandy.vercel.app/users/${_id}/${friendId}`,
+      `https://deploy-mern-api-weld.vercel.app/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
